f766ec6d3dd6c096ef2781fa617389f84b8b73ca
Komal Meena <capkomal2203@gmail.com>
2025-06-19 19:31:21 +0530

    will try gradient now

--- app/page.js
+++ app/page.js
@@ -1,6 +1,5 @@
 import HeroSection from "@/components/hero";
 import { Button } from "@/components/ui/button";
-import HeroSection from "@/components/hero";
 export default function Home() {
   return (
     <div className="mt-40">
