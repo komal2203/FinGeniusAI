19d950a298f4136df95f70667153375062777b49
Komal Meena <capkomal2203@gmail.com>
2025-06-19 16:29:56 +0530

    integrated clerk

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/header";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -12,17 +14,20 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`${inter.className}`}>
-        {/* header */}
-        {children}
-        {/* footer */}
-        <footer>
-          <div>
-            <p>Made with ❤️ by Komal</p>
-          </div>
-        </footer>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className}`}>
+          {/* header */}
+          <Header />
+          <main className="min-h-screen">{children}</main>
+          {/* footer */}
+          <footer className="bg-blue-50 py-12">
+            <div className="container mx-auto px-4 text-center text-gray-600  ">
+              <p>Made with ❤️ by Komal</p>
+            </div>
+          </footer>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
