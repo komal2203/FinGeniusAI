bdc784d192929e9970494a26853284717cdf9877
Komal Meena <capkomal2203@gmail.com>
2025-06-20 15:16:14 +0530

    made hero page and sections ....will now start with database setup

--- app/layout.js
+++ app/layout.js
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
           <Header />
           <main className="min-h-screen">{children}</main>
           {/* footer */}
-          <footer className="bg-blue-50 py-12">
+          <footer className="bg-purple-50 py-3">
             <div className="container mx-auto px-4 text-center text-gray-600  ">
               <p>Made with ❤️ by Komal</p>
             </div>
--- components/hero.jsx
+++ components/hero.jsx
@@ -7,7 +7,7 @@ const HeroSection = () => {
   return (
     <div className="pb-20 px-4">
       <div className="container mx-auto text-center ">
-        <h1 className="text-5xl md:text-8xl lg:text-[105px] pb-6 bg-gradient-to-br from-blue-700  to-green-600 text-transparent bg-clip-text tracking-tighter font-bold pr-2">
+        <h1 className="text-5xl md:text-8xl lg:text-[105px] pb-6 bg-gradient-to-br from-blue-700  to-purple-500 text-transparent bg-clip-text tracking-tighter font-bold pr-2">
           Your Financial Co‑Pilot <br /> Powered by AI
         </h1>
         <p className="text-xl text-gray-600 mb-8 max-w-2xl mx-auto">
@@ -18,7 +18,7 @@ const HeroSection = () => {
         </p>
         <div className="flex justify-center space-x-4">
           <Link href="/dashboard">
-            <Button size="lg" className="px-8">
+            <Button size="lg" className="px-8 bg-gradient-to-br from-blue-700  to-purple-500 hover:brightness-130">
               Get Started
             </Button>
           </Link>
@@ -29,12 +29,12 @@ const HeroSection = () => {
           </Link>
         </div>
 
-        <div className="hero-image-wrapper">
+        <div className="hero-image-wrapper pt-6">
           <div>
             <Image
               src="/banner2.avif"
-              width={1280}
-              height={720}
+              width={1100}
+              height={620}
               alt="Dashboard preview"
               className="rounded-lg shadow-2xl border mx-auto"
               priority
