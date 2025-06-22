a5d0c0e9289ee44fea3fd0d85c0f54d6ddc383f8
Komal Meena <capkomal2203@gmail.com>
2025-06-22 14:59:12 +0530

    created drawer

--- components/create-account-drawer.jsx
+++ components/create-account-drawer.jsx
@@ -1,19 +1,23 @@
 "use client"
 
-import React from "react";
+import React, { useState } from "react";
 import { Drawer, DrawerHeader, DrawerContent, DrawerTitle, DrawerDescription, DrawerTrigger } from "./ui/drawer";
 
 const CreateAccountDrawer = ({ children }) => {
+    const [open, setOpen] = useState(false);
     return (
-        <Drawer>
-            <DrawerTrigger>{children}</DrawerTrigger>
+        <Drawer open={open} onOpenChange={setOpen}>
+            <DrawerTrigger asChild>{children}</DrawerTrigger>
             <DrawerContent>
                 <DrawerHeader>
-                    <DrawerTitle>Are you absolutely sure?</DrawerTitle>
+                    <DrawerTitle>Create New Account</DrawerTitle>
                     <DrawerDescription>This action cannot be undone.</DrawerDescription>
                 </DrawerHeader>
+                <div>
+                    <form ></form>
+                </div>
             </DrawerContent>
-        </Drawer>
+        </Drawer >
     )
 };
 
