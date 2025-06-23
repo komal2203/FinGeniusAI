22be5872d8e9ca3fa0eaf185cc8367d24cca2553
Komal Meena <capkomal2203@gmail.com>
2025-06-23 13:00:02 +0530

    implemented recurring transaction cron job

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,9 +1,9 @@
 import { serve } from "inngest/next";
 import { inngest } from "@/lib/inngest/client";
-import { checkBudgetAlert } from "@/lib/inngest/functions";
+import { checkBudgetAlert, processRecurringTransaction, triggerRecurringTransactions } from "@/lib/inngest/functions";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [checkBudgetAlert],
+  functions: [checkBudgetAlert, triggerRecurringTransactions, processRecurringTransaction],
 });
